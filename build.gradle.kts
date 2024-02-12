8a220a3691a7a3868b96fafa1dafe237413bb53c
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-12 16:04:33 +0100

    Oppdatert libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val javaVersion = JavaLanguageVersion.of(21)
-val tilleggsstønaderLibsVersion = "2024.01.31-13.14.525870bbcf52"
-val tilleggsstønaderKontrakterVersion = "2024.02.07-10.15.07cb3f3164e4"
+val tilleggsstønaderLibsVersion = "2024.02.12-15.54.60684ccdf789"
+val tilleggsstønaderKontrakterVersion = "2024.02.10-11.13.224861e5b075"
 val familieProsesseringVersion = "2.20231212093500_bfa0e7c"
 val tokenSupportVersion = "4.1.3"
 
