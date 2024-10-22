a4d1fda17e565c57e314ef4ac0da86089f40450f
Johan Blomgren <blommish@users.noreply.github.com>
2024-10-22 13:58:49 +0200

    Kan ikke sende inn tom liste til repository

--- src/main/kotlin/no/nav/tilleggsstonader/arena/vedtak/VedtakService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/arena/vedtak/VedtakService.kt
@@ -1,5 +1,7 @@
 package no.nav.tilleggsstonader.arena.vedtak
 
+import no.nav.tilleggsstonader.arena.sak.Aktivitet
+import no.nav.tilleggsstonader.arena.sak.Sak
 import no.nav.tilleggsstonader.arena.sak.SakRepository
 import no.nav.tilleggsstonader.kontrakter.arena.vedtak.Rettighet
 import org.springframework.stereotype.Service
@@ -14,15 +16,37 @@ class VedtakService(
     fun hentSakOgVedtak(identer: Set<String>): SakOgVedtak {
         val alleVedtak = vedtakService.finnVedtak(identer, Rettighet.entries.map { it.kodeArena })
         val saker = sakRepository.finnSaker(identer)
-        val vedtakfakta = vedtakRepository.finnVedtakFakta(alleVedtak.map { it.vedtakId }.toSet())
-        val vilkårsvurderinger = vedtakRepository.finnVilkårsvurderinger(alleVedtak.map { it.vedtakId }.toSet())
-        val aktiviteter = sakRepository.finnAktiviteter(saker.mapNotNull { it.saksforhold?.aktivitetId }.toSet())
+        val vedtakIder = alleVedtak.map { it.vedtakId }.toSet()
+
+        val aktiviteter = hentAktiviteter(saker)
         return SakOgVedtak(
             saker = saker,
             vedtak = alleVedtak,
-            vedtakfakta = vedtakfakta,
-            vilkårsvurderinger = vilkårsvurderinger,
+            vedtakfakta = hentVedtakfakta(vedtakIder),
+            vilkårsvurderinger = hentVilkårsvurderinger(vedtakIder),
             aktiviteter = aktiviteter,
         )
+    }
+
+    private fun hentAktiviteter(saker: List<Sak>): List<Aktivitet> {
+        val aktivitetIder = saker.mapNotNull { it.saksforhold?.aktivitetId }.toSet()
+        if (aktivitetIder.isEmpty()) {
+            return emptyList()
+        }
+        return sakRepository.finnAktiviteter(aktivitetIder)
+    }
+
+    private fun hentVilkårsvurderinger(vedtakIder: Set<Int>): List<Vilkårsvurdering> {
+        if (vedtakIder.isEmpty()) {
+            return emptyList()
+        }
+        return vedtakRepository.finnVilkårsvurderinger(vedtakIder)
+    }
+
+    private fun hentVedtakfakta(vedtakIder: Set<Int>): List<Vedtakfakta> {
+        if (vedtakIder.isEmpty()) {
+            return emptyList()
+        }
+        return vedtakRepository.finnVedtakFakta(vedtakIder)
     }
 }
