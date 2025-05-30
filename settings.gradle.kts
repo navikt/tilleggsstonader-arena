dcebf054682f62b50e8a4e62fe89d60ed51c61d0
Johan Blomgren <blommish@users.noreply.github.com>
2025-05-30 10:20:19 +0200

    Tar i bruk version catalog for å styre versjoner på et sted og bundle det som henger sammen

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
 rootProject.name = "tilleggsstonader-arena"
 
+val versionCatalog = "2025.05.30-10.00.fb98fd578374"
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("no.nav.tilleggsstonader:tilleggsstonader-version-catalog:$versionCatalog")
+        }
+    }
 }
