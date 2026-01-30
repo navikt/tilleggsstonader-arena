c1eb0dbccdc6cbd413ede634509504d508b23399
Andreas Strand <andreas.strand@nav.no>
2026-01-30 11:06:47 +0100

    Kotlin 2.2.21 fordi codeql støtter ikke 2.3 enda

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ version = "1.0.0"
 plugins {
     application
 
-    kotlin("jvm") version "2.3.0"
+    kotlin("jvm") version "2.2.21"
     id("com.diffplug.spotless") version "8.0.0"
     id("com.github.ben-manes.versions") version "0.53.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
