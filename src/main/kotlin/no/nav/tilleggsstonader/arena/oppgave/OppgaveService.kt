c244c38c70d45bc8d5c052bd47db1355c8e6053b
Runar Skagestad <runar.skagestad@nav.no>
2026-03-13 11:45:37 +0100

    🔊 Logg ut resultatet fra spørring mot oppgaver i Arena

--- src/main/kotlin/no/nav/tilleggsstonader/arena/oppgave/OppgaveService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/arena/oppgave/OppgaveService.kt
@@ -1,6 +1,7 @@
 package no.nav.tilleggsstonader.arena.oppgave
 
 import no.nav.tilleggsstonader.kontrakter.arena.oppgave.ArenaOppgaveDto
+import no.nav.tilleggsstonader.libs.log.SecureLogger.secureLogger
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,8 +13,16 @@ class OppgaveService(
             identer.flatMap { ident ->
                 listOf("TS:$ident", "TA:$ident")
             }
+        secureLogger.info("Gjør spørring mot taskInstance for identer=$identliste")
         return oppgaveRepository
             .hentOppgaver(identliste)
             .map { it.tilOppgaveArena() }
+            .also {
+                secureLogger.info(
+                    "Fant ${it.size} oppgaver med titler ${
+                        it.joinToString { oppgave -> oppgave.tittel }
+                    }",
+                )
+            }
     }
 }
