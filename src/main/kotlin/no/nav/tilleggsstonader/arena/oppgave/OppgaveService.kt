b50ec7ed25b2d6f7e692be8ca2c118c2734f310f
Adnan Vrevic <adnan.vrevic@nav.no>
2026-04-17 13:00:54 +0200

    🔧 Fjern unødvendig logging i OppgaveService

--- src/main/kotlin/no/nav/tilleggsstonader/arena/oppgave/OppgaveService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/arena/oppgave/OppgaveService.kt
@@ -1,7 +1,6 @@
 package no.nav.tilleggsstonader.arena.oppgave
 
 import no.nav.tilleggsstonader.kontrakter.arena.oppgave.ArenaOppgaveDto
-import no.nav.tilleggsstonader.libs.log.SecureLogger.secureLogger
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,16 +12,8 @@ class OppgaveService(
             identer.flatMap { ident ->
                 listOf("TS:$ident", "TA:$ident")
             }
-        secureLogger.info("Gjør spørring mot taskInstance for identer=$identliste")
         return oppgaveRepository
             .hentOppgaver(identliste)
             .map { it.tilOppgaveArena() }
-            .also {
-                secureLogger.info(
-                    "Fant ${it.size} oppgaver med titler ${
-                        it.joinToString { oppgave -> oppgave.tittel }
-                    }",
-                )
-            }
     }
 }
